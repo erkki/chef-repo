c0187f4920bece895899b5a9b644e8d2c2723e14
Erkki Eilonen <erkki@itech.ee>
2009-12-25 17:09:21 +0200

    add quick_start

---
+++ cookbooks/quick_start/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "iTech Solutions TÜ"
+maintainer_email "erkki@itech.ee"
+license          "All rights reserved"
+description      "Installs/Configures quick_start"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.1"
